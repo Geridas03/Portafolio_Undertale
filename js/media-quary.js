6939079cc02577e6da0e0505fbda0b3888bdff72
Gerida03 <ericksantiro03@gmail.com>
2023-06-09 08:07:24 -0500

    fix dir css

---
+++ js/media-quary.js
@@ -0,0 +1,18 @@
+let mainNav = document.getElementById("mainNav")
+let menuBar = document.getElementById("menuBar")
+let mainMenu = document.getElementById("mainMenu");
+
+
+menuBar.addEventListener("click", function() {
+    if (mainMenu.classList.contains("main-menu")) {
+        mainMenu.classList.add("main-menu-block");
+        mainMenu.classList.remove("main-menu");
+    }
+    else {
+        mainMenu.classList.add("main-menu");
+        mainMenu.classList.remove("main-menu-block")
+    }
+    mainMenu.classList.remove("main-nav");
+    mainMenu.classList.add("main-nav-2")
+});
+alert("Bienvenidos")
