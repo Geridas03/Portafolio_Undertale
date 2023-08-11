122b45435929ae672078d36c5156f8e35eabb24d
Gerida03 <ericksantiro03@gmail.com>
2023-08-11 11:08:07 -0500

    Maqueta

--- js/maquetacion2.js
+++ js/maquetacion2.js
@@ -1,14 +1,19 @@
-let menuBar = document. getElementById("menubar")
-let mainNav = document. getElementById("mainNav")
-let mainMenu = document. getElementById("mainMenu")
+let mainNav = document.getElementById("mainNav")
+let menuBar = document.getElementById("menuBar")
+let mainMenu = document.getElementById("mainMenu");
 
-menuBar.addEventListener("clic", function(){
-    if(mainMenu.classList.contains("main-menu")){
+
+menuBar.addEventListener("click", function() {
+    if (mainMenu.classList.contains("main-menu-pc")) {
         mainMenu.classList.add("main-menu-block");
-        mainMenu.classList.remove("main-meun")
+        mainMenu.classList.remove("main-menu-pc");
+        mainMenu.classList.remove("main-menu-block-hidden");
     }
     else {
-        mainMenu.classList.add("main-menu");
-        mainMenu.classList.remove("main-meu-block");
+        mainMenu.classList.add("main-menu-pc");
+        mainMenu.classList.add("main-menu-block-hidden");
+        mainMenu.classList.remove("main-menu-block");
     }
+    mainMenu.classList.remove("main-nav");
+    mainMenu.classList.add("main-nav-2");
 });
